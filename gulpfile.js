ad1efa8097f4ef501824391ab60c1ab934889dbd
daixianfeng <xianfeng_dai@outlook.com>
2018-11-26 15:52:30 +0800

    tmp save caster

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var rename = require("gulp-rename");
+var less = require('gulp-less');
+var path = require('path');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var clean = require('gulp-clean');
+
+
+gulp.task('webserver', function(){
+    gulp.src('./').pipe(webserver({
+            port: 8802,//端口
+            host: '127.0.0.1',//域名
+            liveload: false,//实时刷新代码。不用f5刷新
+            directoryListing: {
+                path: './',
+                enable: true
+            }
+        }));
+});
+
+var version = '1.0.0';
+gulp.task('clean', function(){
+    return gulp.src('./dist/'+version)
+        .pipe(clean({force: true}));
+});
+
+gulp.task('css', function(){
+    return gulp.src('./src/**/*.css')
+        .pipe(gulp.dest('./dist/'+version+'/'))
+        .pipe(rename({
+            suffix: '.min',
+            extname: '.css'
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./dist/'+version+'/'));
+});
+gulp.task('less', function(){
+    return gulp.src(['./src/theme/**/*.less','./src/caster.less'])
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(gulp.dest('./dist/'+version+'/'))
+        .pipe(rename({
+            suffix: '.min',
+            extname: '.css'
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./dist/'+version+'/'));
+});
+
+gulp.task('default', ['less','css']);
